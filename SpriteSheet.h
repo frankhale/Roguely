3ce1076df4de00dc3d3af54ca6fed16021e43fed
Frank Hale <frankhale@gmail.com>
2021-02-28 19:21:01 -0500

    cleanup main.cpp

--- SpriteSheet.h
+++ SpriteSheet.h
@@ -7,41 +7,40 @@
 #define NUM_SPRITES 19
 
 enum Sprites {
-    WALL = 0,
-    HEALTH_GEM = 1,
-    ATTACK_BONUS_GEM = 2,
-    PLAYER = 3,
-    SPIDER = 4,
-    LURCHER = 5,
-    GOLDEN_CANDLE = 6,
-    DOWN_LADDER = 7,
-    UP_LADDER = 8,
-    GROUND = 9,
-    WATER = 10,
-    GRASS = 11,
-    CRAB = 12,
-    TREASURE_CHEST = 13,
-    COIN = 14,
-    DOOR = 15,
-    WALL_SECRET_DOOR = 16,
-    BUG = 17,
-    HIDDEN = 18
+		WALL = 0,
+		HEALTH_GEM = 1,
+		ATTACK_BONUS_GEM = 2,
+		PLAYER = 3,
+		SPIDER = 4,
+		LURCHER = 5,
+		GOLDEN_CANDLE = 6,
+		DOWN_LADDER = 7,
+		UP_LADDER = 8,
+		GROUND = 9,
+		WATER = 10,
+		GRASS = 11,
+		CRAB = 12,
+		TREASURE_CHEST = 13,
+		COIN = 14,
+		DOOR = 15,
+		WALL_SECRET_DOOR = 16,
+		BUG = 17,
+		HIDDEN = 18
 };
 
 class SpriteSheet
 {
 public:
-    SpriteSheet(SDL_Renderer *renderer, const char* path, int sw, int sh);
-    ~SpriteSheet();
+		SpriteSheet(SDL_Renderer* renderer, const char* path, int sw, int sh);
+		~SpriteSheet();
 
-    void drawSprite(SDL_Renderer *renderer, int sprite_id, int x, int y);
-    int totalSprites() const { return NUM_SPRITES; }
+		void drawSprite(SDL_Renderer* renderer, int sprite_id, int x, int y);
+		int totalSprites() const { return NUM_SPRITES; }
 
-    SDL_Texture* get_spritesheet_texture() const { return spritesheet_texture; }
+		SDL_Texture* get_spritesheet_texture() const { return spritesheet_texture; }
 
 private:
-    int sprite_width, sprite_height;
-    SDL_Rect sprites[NUM_SPRITES];
-    SDL_Texture* spritesheet_texture;
+		int sprite_width, sprite_height;
+		SDL_Rect sprites[NUM_SPRITES];
+		SDL_Texture* spritesheet_texture;
 };
-
--- main.cpp
+++ main.cpp
@@ -12,13 +12,26 @@
 
 const int WINDOW_WIDTH = 1056;
 const int WINDOW_HEIGHT = 768;
+const bool MUSIC = false;
 
 const std::string WINDOW_ICON_PATH = "assets/icon.png";
 const std::string GAME_TILESET_PATH = "assets/tileset2.png";
 const std::string FONT_PATH = "assets/VT323-Regular.ttf";
-const std::string MUSIC = "assets/ExitExitProper.mp3";
+const std::string MUSIC_PATH = "assets/ExitExitProper.mp3";
 
-int main(int argc, char* args[])
+SDL_Window* window = nullptr;
+SDL_Surface* window_surface = nullptr;
+SDL_Renderer* renderer = nullptr;
+Mix_Music* mix_music = nullptr;
+
+std::shared_ptr<Game> game = nullptr;
+std::shared_ptr<SpriteSheet> sprite_sheet = nullptr;
+std::shared_ptr<Text> text = nullptr;
+std::ostringstream player_health;
+std::ostringstream player_score;
+std::ostringstream enemies_killed;
+
+int init_sdl(std::string window_title)
 {
 		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
 		{
@@ -44,7 +57,7 @@ int main(int argc, char* args[])
 				return -1;
 		}
 
-		SDL_Window* window = SDL_CreateWindow("SDL2 Roguelike",
+		window = SDL_CreateWindow(window_title.c_str(),
 				SDL_WINDOWPOS_CENTERED,
 				SDL_WINDOWPOS_CENTERED,
 				WINDOW_WIDTH, WINDOW_HEIGHT,
@@ -56,9 +69,11 @@ int main(int argc, char* args[])
 				return -1;
 		}
 
-		SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+		window_surface = SDL_GetWindowSurface(window);
+
 		SDL_Surface* window_icon_surface = IMG_Load(WINDOW_ICON_PATH.c_str());
 		SDL_SetWindowIcon(window, window_icon_surface);
+		SDL_FreeSurface(window_icon_surface);
 
 		if (!window_surface)
 		{
@@ -66,23 +81,148 @@ int main(int argc, char* args[])
 				return -1;
 		}
 
-		SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
-		SDL_Renderer* icon_renderer = SDL_CreateRenderer(window, -1, 0);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
-		auto game = std::make_shared<Game>();
-		auto sprite_sheet = std::make_shared<SpriteSheet>(renderer, GAME_TILESET_PATH.c_str(), 24, 24);
-		auto text = std::make_shared<Text>();
+		if (MUSIC)
+		{
+				Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 4096);
+				mix_music = Mix_LoadMUS(MUSIC_PATH.c_str());
+				Mix_Volume(-1, 5);
+				Mix_VolumeMusic(5);
+				Mix_PlayMusic(mix_music, 1);
+		}
+
+		return 0;
+}
+
+void tear_down_sdl()
+{		
+		if (MUSIC)
+		{
+				Mix_FreeMusic(mix_music);
+		}
+
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+
+		Mix_Quit();
+		TTF_Quit();
+		IMG_Quit();
+		SDL_Quit();
+}
+
+void init_game()
+{
+		game = std::make_shared<Game>();
+		sprite_sheet = std::make_shared<SpriteSheet>(renderer, GAME_TILESET_PATH.c_str(), 24, 24);
+		text = std::make_shared<Text>();
 		text->LoadFont(FONT_PATH.c_str(), 28);
+}
 
-		std::ostringstream player_health;
-		std::ostringstream player_score;
-		std::ostringstream enemies_killed;
+void render_game()
+{
+		if (strlen(game->GetWinLoseMessage().c_str()) <= 1)
+		{
+				for (int r = 0; r < game->GetViewPortHeight(); r++)
+				{
+						for (int c = 0; c < game->GetViewPortWidth(); c++)
+						{
+								int dx = (c * 24) - (game->GetViewPortX() * 24);
+								int dy = (r * 24) - (game->GetViewPortY() * 24);
 
-		Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 4096);
-		Mix_Music* music = Mix_LoadMUS(MUSIC.c_str());
-		Mix_Volume(-1, 5);
-		Mix_VolumeMusic(5);
-		Mix_PlayMusic(music, 1);
+								if (game->LightMap()[r][c] == 2)
+								{
+										sprite_sheet->drawSprite(renderer, game->Map()[r][c], dx, dy);
+
+										for (auto& elem : **game->GetCoins())
+										{
+												if (elem.point.x == c &&
+														elem.point.y == r)
+												{
+														sprite_sheet->drawSprite(renderer, COIN, dx, dy);
+												}
+										}
+
+										for (auto& elem : **game->GetHealthGems())
+										{
+												if (elem.point.x == c &&
+														elem.point.y == r)
+												{
+														sprite_sheet->drawSprite(renderer, HEALTH_GEM, dx, dy);
+												}
+										}
+
+										for (auto& elem : **game->GetEnemies())
+										{
+												if (elem.point.x == c &&
+														elem.point.y == r)
+												{
+														int enemy_id = 0;
+
+														if (elem.id == 50) enemy_id = SPIDER;
+														else if (elem.id == 51)enemy_id = LURCHER;
+														else if (elem.id == 52)enemy_id = CRAB;
+														else if (elem.id == 53)enemy_id = BUG;
+
+														sprite_sheet->drawSprite(renderer, enemy_id, dx, dy);
+												}
+										}
+
+										for (auto& elem : **game->GetTreasureChests())
+										{
+												if (elem.point.x == c &&
+														elem.point.y == r)
+												{
+														sprite_sheet->drawSprite(renderer, TREASURE_CHEST, dx, dy);
+												}
+										}
+
+										auto golden_candle = game->GetGoldenCandle();
+										if (golden_candle.point.x == c && golden_candle.point.y == r)
+										{
+												sprite_sheet->drawSprite(renderer, GOLDEN_CANDLE, dx, dy);
+										}
+
+										if (r == game->GetPlayerY() && c == game->GetPlayerX())
+										{
+												sprite_sheet->drawSprite(renderer, 3, dx, dy);
+										}
+								}
+								else
+								{
+										sprite_sheet->drawSprite(renderer, HIDDEN, dx, dy);
+								}
+						}
+				}
+		}
+		else
+		{
+				text->DrawText(renderer, 10, 10, game->GetWinLoseMessage().c_str());
+		}
+
+		player_health << "Health: " << game->GetPlayerHealth();
+		player_score << "Score: " << game->GetPlayerScore();
+		enemies_killed << "Enemies Killed: " << game->GetPlayerEnemiesKilled();
+
+		text->DrawText(renderer, 10, WINDOW_HEIGHT - 7 * 20, player_health.str().c_str());
+		text->DrawText(renderer, 10, WINDOW_HEIGHT - 5 * 20, player_score.str().c_str());
+		text->DrawText(renderer, 10, WINDOW_HEIGHT - 3 * 20, enemies_killed.str().c_str());
+
+		text->DrawText(renderer, 300, WINDOW_HEIGHT - 7 * 20, game->GetPlayerCombatInfo().c_str());
+		text->DrawText(renderer, 300, WINDOW_HEIGHT - 5 * 20, game->GetEnemyStatInfo().c_str());
+		text->DrawText(renderer, 300, WINDOW_HEIGHT - 3 * 20, game->GetEnemyCombatInfo().c_str());
+}
+
+int main(int argc, char* args[])
+{
+		auto result = init_sdl("SDL2 Roguelike");
+
+		if (result < 0)
+		{
+				return -1;
+		}
+
+		init_game();
 
 		bool keep_window_open = true;
 		while (keep_window_open)
@@ -90,13 +230,6 @@ int main(int argc, char* args[])
 				SDL_Event e;
 				while (SDL_PollEvent(&e) > 0)
 				{
-						player_health.str("");
-						player_score.str("");
-						enemies_killed.str("");
-
-						SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-						SDL_RenderClear(renderer);
-
 						switch (e.type)
 						{
 						case SDL_QUIT:
@@ -121,113 +254,22 @@ int main(int argc, char* args[])
 								}
 								break;
 						}
-
-						if (strlen(game->GetWinLoseMessage().c_str()) <= 1)
-						{
-								for (int r = 0; r < game->GetViewPortHeight(); r++)
-								{
-										for (int c = 0; c < game->GetViewPortWidth(); c++)
-										{
-												int dx = (c * 24) - (game->GetViewPortX() * 24);
-												int dy = (r * 24) - (game->GetViewPortY() * 24);
-
-												if (game->LightMap()[r][c] == 2)
-												{
-														sprite_sheet->drawSprite(renderer, game->Map()[r][c], dx, dy);
-
-														for (auto& elem : **game->GetCoins())
-														{
-																if (elem.point.x == c &&
-																		elem.point.y == r)
-																{
-																		sprite_sheet->drawSprite(renderer, COIN, dx, dy);
-																}
-														}
-
-														for (auto& elem : **game->GetHealthGems())
-														{
-																if (elem.point.x == c &&
-																		elem.point.y == r)
-																{
-																		sprite_sheet->drawSprite(renderer, HEALTH_GEM, dx, dy);
-																}
-														}
-
-														for (auto& elem : **game->GetEnemies())
-														{
-																if (elem.point.x == c &&
-																		elem.point.y == r)
-																{
-																		int enemy_id = 0;
-
-																		if (elem.id == 50) enemy_id = SPIDER;
-																		else if (elem.id == 51)enemy_id = LURCHER;
-																		else if (elem.id == 52)enemy_id = CRAB;
-																		else if (elem.id == 53)enemy_id = BUG;
-
-																		sprite_sheet->drawSprite(renderer, enemy_id, dx, dy);
-																}
-														}
-
-														for (auto& elem : **game->GetTreasureChests())
-														{
-																if (elem.point.x == c &&
-																		elem.point.y == r)
-																{
-																		sprite_sheet->drawSprite(renderer, TREASURE_CHEST, dx, dy);
-																}
-														}
-
-														auto golden_candle = game->GetGoldenCandle();
-														if (golden_candle.point.x == c && golden_candle.point.y == r)
-														{
-																sprite_sheet->drawSprite(renderer, GOLDEN_CANDLE, dx, dy);
-														}
-
-														if (r == game->GetPlayerY() && c == game->GetPlayerX())
-														{
-																sprite_sheet->drawSprite(renderer, 3, dx, dy);
-														}
-												}
-												else
-												{
-														sprite_sheet->drawSprite(renderer, HIDDEN, dx, dy);
-												}
-										}
-								}
-						}
-						else
-						{
-								text->DrawText(renderer, 10, 10, game->GetWinLoseMessage().c_str());
-						}
-
-						player_health << "Health: " << game->GetPlayerHealth();
-						player_score << "Score: " << game->GetPlayerScore();
-						enemies_killed << "Enemies Killed: " << game->GetPlayerEnemiesKilled();
-
-						text->DrawText(renderer, 10, WINDOW_HEIGHT - 7 * 20, player_health.str().c_str());
-						text->DrawText(renderer, 10, WINDOW_HEIGHT - 5 * 20, player_score.str().c_str());
-						text->DrawText(renderer, 10, WINDOW_HEIGHT - 3 * 20, enemies_killed.str().c_str());
-
-						text->DrawText(renderer, 300, WINDOW_HEIGHT - 7 * 20, game->GetPlayerCombatInfo().c_str());
-						text->DrawText(renderer, 300, WINDOW_HEIGHT - 5 * 20, game->GetEnemyStatInfo().c_str());
-						text->DrawText(renderer, 300, WINDOW_HEIGHT - 3 * 20, game->GetEnemyCombatInfo().c_str());
-
-						SDL_RenderPresent(renderer);
-						SDL_Delay(1000 / 60);
 				}
+
+				player_health.str("");
+				player_score.str("");
+				enemies_killed.str("");
+
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+				SDL_RenderClear(renderer);
+
+				render_game();
+
+				SDL_RenderPresent(renderer);
+				SDL_Delay(1000 / 60);
 		}
 
-		Mix_FreeMusic(music);
-		SDL_FreeSurface(window_icon_surface);
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyRenderer(icon_renderer);
-		SDL_DestroyWindow(window);
-
-		Mix_Quit();
-		TTF_Quit();
-		IMG_Quit();
-		SDL_Quit();
+		tear_down_sdl();
 
 		return 0;
 }
