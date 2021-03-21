71f2d0594d1fa58d1a92f2b572819b88f9e1d853
Frank Hale <frankhale@gmail.com>
2021-03-21 02:53:13 -0400

    add license to Lua script

--- game.lua
+++ game.lua
@@ -1,3 +1,27 @@
+-- game.lua
+--
+-- MIT License
+--
+-- Copyright (c) 2021 Frank Hale
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 Game = {
 	window_title = "Roguely - A simple Roguelike in C++/Lua/SDL2",
 	window_icon_path = "assets/icon.png",
@@ -315,6 +339,8 @@ function initiate_attack_sequence(pid)
 	local enemy_attack = Game.enemies[pid].enemy.components.stats_component.attack
 	local enemy_health = Game.enemies[pid].enemy.components.health_component.health
 	local enemy_id = Game.enemies[pid].enemy.id
+
+	-- TODO: Add combat text
 
 	-- player strikes first
 	if (player_crit_chance) then
